e63128b1903da9a56425a3dc26985b2b50efd606
arman-yekkehkhani <arman.yekkehkhani@gmail.com>
2024-09-16 01:23:57 +0330

    add repos

---
+++ src/main/java/edu/personal/report/repository/ReportRepository.java
@@ -0,0 +1,14 @@
+package edu.personal.report.repository;
+
+import edu.personal.report.model.Report;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, UUID> {
+    Optional<Report> findByUuid(UUID uuid);
+}
